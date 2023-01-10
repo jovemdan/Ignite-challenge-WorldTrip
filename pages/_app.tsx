a8a79ddf6c03b7ef1b0d31d113d818500d1786e7
Danilo Goncalves de Menezes <danilo.menezes@guide.com.br>
2023-01-09 23:45:34 -0300

    feat: Add theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider, theme } from '@chakra-ui/react'
+import { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
+
+export default MyApp
