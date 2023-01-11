6b4d04182051ebacee82ebd9303416c269e947e4
Danilo Goncalves de Menezes <danilo.menezes@guide.com.br>
2023-01-11 17:18:32 -0300

    feat: Responsive continent text

--- components/chooseContinentText.tsx
+++ components/chooseContinentText.tsx
@@ -3,14 +3,14 @@ import { Center, Text } from '@chakra-ui/react'
 export function ChooseContinentText() {
   return (
     <>
-      <Center pt='40'>
-        <Text as='h1' color='gray.600' fontSize='36px'>
+      <Center pt={['10', '40']}>
+        <Text as='h1' color='gray.600' fontSize={['20px', '36px']}>
           Vamos nessa?
         </Text>
         <br />
       </Center>
       <Center>
-        <Text as='h1' color='gray.600' fontSize='36px'>
+        <Text as='h1' color='gray.600' fontSize={['20px', '36px']}>
           Então escolha seu continente
         </Text>
       </Center>
