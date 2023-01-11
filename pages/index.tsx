b98cb334e7a4b0328b1c5966e3895ba2c2e179c4
Danilo Goncalves de Menezes <danilo.menezes@guide.com.br>
2023-01-10 22:06:50 -0300

    feat: Clean file, put components

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,15 +7,15 @@ import {
   Spacer,
   HStack,
 } from '@chakra-ui/react'
+import { Banner } from '../components/banner'
+import { ChooseContinentText } from '../components/chooseContinentText'
+import { Header } from '../components/header'
+import { IconsInfo } from '../components/icons-info'
 
 export default function Home() {
   return (
     <>
-      <Box as='header' w='100%' h='20'>
-        <Center pt='27'>
-          <Image src='/Logo.png' alt='logo' />
-        </Center>
-      </Box>
+      <Header />
 
       <Box
         mt='27'
@@ -24,38 +24,10 @@ export default function Home() {
         backgroundSize='100% auto'
         h={['163px', '335px']}
       >
-        <Flex
-          maxW='1160px'
-          // justifyContent='space-between'
-          alignContent='center'
-          alignItems='center'
-          margin='0 auto'
-          gap='32'
-        >
-          <Box>
-            <Flex
-              h='100%'
-              flexDirection='column'
-              justifyContent='center'
-              paddingX={['16px', '0px']}
-            >
-              <Text fontSize='36' color='gray.50' fontFamily='Poppins'>
-                5 Continentes,
-                <br /> infinitas possibilidades.
-              </Text>
-              <Text
-                fontSize='20'
-                color='#DADADA'
-                fontWeight='400'
-                fontFamily='Poppins'
-                pt='5'
-              >
-                Chegou a hora de tirar do papel a viagem que você sempre sonhou.
-              </Text>
-            </Flex>
-          </Box>
-          <Image src='/Airplane.svg' alt='airplane' h='355px' mt='20px' />
-        </Flex>
+        <Banner />
+        <IconsInfo />
+
+        <ChooseContinentText />
       </Box>
     </>
   )
