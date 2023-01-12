c73424a1e281660b1a9df13a0fdd9c23317751ea
Danilo Goncalves de Menezes <danilo.menezes@guide.com.br>
2023-01-12 16:43:17 -0300

    feat: Adjust Responsive device Header

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,7 @@ export function Header() {
   return (
     <>
       <Box as='header' w='100%' h='20'>
-        <Center pt={['6', '6', '27']}>
+        <Center pt={['6', '6', '4']}>
           <Image h={['30', '30', '45']} src='/Logo.png' alt='logo' />
         </Center>
       </Box>
