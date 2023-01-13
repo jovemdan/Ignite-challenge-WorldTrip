64b6cf2bdd91a1671631f5dd5697d324bad2044f
Danilo Goncalves de Menezes <danilo.menezes@guide.com.br>
2023-01-13 17:00:17 -0300

    feat: adjusts header and add o icon back

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,26 @@
-import { Box, Center, Image } from '@chakra-ui/react'
+import { ChevronLeftIcon } from '@chakra-ui/icons'
+import { Box, Center, Flex, Image } from '@chakra-ui/react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export function Header() {
+  const router = useRouter()
+
   return (
     <>
       <Box as='header' w='100%' h='20'>
-        <Center pt={['6', '6', '4']}>
+        <Flex pt={['6', '6', '4']} justify='center' alignItems='center'>
+          {router.pathname !== '/' && (
+            <Link href='/'>
+              <ChevronLeftIcon
+                style={{ cursor: 'pointer', position: 'absolute', left: 40 }}
+                w={8}
+                h={8}
+              />
+            </Link>
+          )}
           <Image h={['30', '30', '45']} src='/Logo.png' alt='logo' />
-        </Center>
+        </Flex>
       </Box>
     </>
   )
