0f75da12a6bac99875f0f91d673013778b96b6c5
Danilo Goncalves de Menezes <danilo.menezes@guide.com.br>
2023-01-16 20:16:57 -0300

    feat: Implementing useBreakpointValue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,24 @@
-import {
-  Image,
-  Box,
-  Center,
-  Text,
-  Flex,
-  Spacer,
-  HStack,
-} from '@chakra-ui/react'
+import { Box, useBreakpointValue } from '@chakra-ui/react'
 import { Banner } from '../components/banner'
 import { BannerSlider } from '../components/banner-slider'
 import { ChooseContinentText } from '../components/chooseContinentText'
 import { Header } from '../components/header'
 import { IconsInfo } from '../components/icons-info'
+import { IconsInfoMobile } from '../components/icons-info-mobile'
 
 export default function Home() {
+  const isMobileVersion = useBreakpointValue({
+    base: true,
+    md: false,
+    lg: false,
+  })
   return (
     <>
       <Header />
-      <Box minHeight='1440px'>
+      <Box height={['900px', '1200px', '1469px']}>
         <Banner />
-        <IconsInfo />
+        {isMobileVersion ? <IconsInfoMobile /> : <IconsInfo />}
         <ChooseContinentText />
-
         <BannerSlider />
       </Box>
     </>
