1f4cfc3190f7e48c1fa2e1d299f533410ded2d7d
Danilo Goncalves de Menezes <danilo.menezes@guide.com.br>
2023-02-07 21:43:16 -0300

    fix: Clean code

--- components/cities.tsx
+++ components/cities.tsx
@@ -1,95 +1,45 @@
-import { Box, Flex, Text } from '@chakra-ui/react'
+import { Avatar, Box, Flex, Text, VStack } from '@chakra-ui/react'
 
 interface Cities {
   bgImage: string[]
 }
 export function Cities(props: Cities) {
   return (
-    <Box mt='80px' fontSize='30px' fontWeight='500' lineHeight='54px'>
-      <Text color='#47585B'>Cidades +100</Text>
-      <Flex gap='30px' flexWrap='wrap' mt='40px'>
-        <Flex flexDirection='column'>
-          <Box
-            w='250px'
-            h='173px'
-            borderRadius='4px 4px 0px 0px'
-            bgSize='cover'
-            bgRepeat='no-repeat'
-            bgImage={props.bgImage}
-          ></Box>
-          <Box
-            w='250px'
-            h='106px'
-            bgColor='white'
-            borderRadius='0px 0px 4px 4px'
-          ></Box>
-        </Flex>
+    <Flex flexDirection='column'>
+      <Box
+        w='250px'
+        h='173px'
+        borderRadius='4px 4px 0px 0px'
+        bgSize='cover'
+        bgRepeat='no-repeat'
+        bgImage={props.bgImage}
+      ></Box>
+      <Box w='250px' h='106px' bgColor='white' borderRadius='0px 0px 4px 4px'>
+        <Flex flexDirection='column' marginInline='24px' mt='18px'>
+          <Flex justifyContent='space-between'>
+            <Flex flexDirection='column' gap='12px'>
+              <Text
+                fontSize='20px'
+                lineHeight='25px'
+                fontWeight='600'
+                color='#47585B'
+              >
+                Londres
+              </Text>
+              <Text
+                fontSize='16px'
+                lineHeight='25px'
+                fontWeight='500'
+                color='#999999'
+              >
+                Reino Unido
+              </Text>
+            </Flex>
 
-        <Flex flexDirection='column'>
-          <Box
-            w='250px'
-            h='173px'
-            bgSize='cover'
-            bgRepeat='no-repeat'
-            bgImage={props.bgImage}
-            borderRadius='4px 4px 0px 0px'
-          ></Box>
-          <Box
-            w='250px'
-            h='106px'
-            bgColor='white'
-            borderRadius='0px 0px 4px 4px'
-          ></Box>
+            <Avatar w='30px' h='30px' mt='15px'></Avatar>
+          </Flex>
         </Flex>
-        <Flex flexDirection='column'>
-          <Box
-            w='250px'
-            h='173px'
-            bgSize='cover'
-            bgRepeat='no-repeat'
-            bgImage={props.bgImage}
-            borderRadius='4px 4px 0px 0px'
-          ></Box>
-          <Box
-            w='250px'
-            h='106px'
-            bgColor='white'
-            borderRadius='0px 0px 4px 4px'
-          ></Box>
-        </Flex>
-        <Flex flexDirection='column'>
-          <Box
-            w='250px'
-            h='173px'
-            bgSize='cover'
-            bgRepeat='no-repeat'
-            bgImage={props.bgImage}
-            borderRadius='4px 4px 0px 0px'
-          ></Box>
-          <Box
-            w='250px'
-            h='106px'
-            bgColor='white'
-            borderRadius='0px 0px 4px 4px'
-          ></Box>
-        </Flex>
-        <Flex flexDirection='column'>
-          <Box
-            w='250px'
-            h='173px'
-            bgSize='cover'
-            bgRepeat='no-repeat'
-            bgImage={props.bgImage}
-            borderRadius='4px 4px 0px 0px'
-          ></Box>
-          <Box
-            w='250px'
-            h='106px'
-            bgColor='white'
-            borderRadius='0px 0px 4px 4px'
-          ></Box>
-        </Flex>
-      </Flex>
-    </Box>
+      </Box>
+    </Flex>
   )
 }
